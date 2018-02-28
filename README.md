e4e7ea2b539a8b117472df39a2939e6f75670dfd
Fabian Pflug <pflug@chi.uni-hannover.de>
2018-02-28 18:55:52 +0100

    Initial commit
    A working version. Not much to do

---
+++ README.md
@@ -0,0 +1,15 @@
+# Train delay
+
+A simple program to check when the next train is coming and if is has any delay
+
+## Adjust to own needs
+1. Go to https://reiseauskunft.bahn.de and put in your start and destination train
+station using auto complete.
+2. After that, inspect the website to see what exactly
+are the values for the fields with the source and destination train station.
+3. Copy these magic numbers (Some obscure long string, containing the name, the ID
+of the train station and something else I did not understand) into the source
+code and you are good to go.
+4. Done
+
+Have fun
---
+++ schedule.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+"""Train delay from my home to the main station
+
+This file parses the website of Deutsch Bahn to check if any trains are late
+and prints out the next trains from my home to the main station including
+their delay
+"""
+
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+
+
+def has_the_right_class(tag):
+    """Selector for the field of the train delay"""
+    return tag.has_attr('class') and "time" in tag['class'] \
+        and tag.parent.has_attr('class') and "firstrow" in tag.parent["class"]
+
+
+def main():
+    """Main method. query website and prints times """
+    url = 'https://reiseauskunft.bahn.de/bin/query.exe/dn'
+    post_fields = {
+        'REQ0JourneyStopsS0ID': u'A=1@O=Empelde@X=9668986@Y=52340160' +
+        '@U=81@L=008001781@B=1@p=1516311829@',
+        'REQ0JourneyStopsZ0ID': u'A=1@O=Hannover Hbf@X=9741017@Y=52376764@' +
+        'U=80@L=008000152@B=1@p=1511926111@',
+        'start': 'Suchen'
+    }
+    request = Request(url, urlencode(post_fields).encode())
+    html_doc = urlopen(request).read()
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    for link in soup.find_all(has_the_right_class):
+        print("time " + link.get_text().strip())
+
+
+if __name__ == "__main__":
+    main()
